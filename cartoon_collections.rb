ce95e27e3979b75e2143339b59032dfea2a319ee
Patricia Carter <patricia4194@gmail.com>
2018-08-08 00:45:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,36 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf_names)
+  dwarf_names.each_with_index do |name, index|
+    puts "#{index+1}. #{name}"
+  end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect do |planeteer|
+    planeteer.capitalize << '!'
+  end 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def select_method(array)
+  array.select do |x|
+    x.length > 4
+  end
+end 
+
+def long_planeteer_calls(planeteers)
+  if select_method(planeteers) == []
+    return false 
+  else 
+    return true 
+  end 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ingredients)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese_types.find do |dacheese|
+    ingredients.find do |dafood|
+      dafood.include?(dacheese)
+    end 
+  end 
+    
 end
