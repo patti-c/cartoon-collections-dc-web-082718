b39516826b17a926b7a3e163a4b4ce6d90259bda
Patricia Carter <patricia4194@gmail.com>
2018-08-08 00:33:09 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,6 +14,10 @@ def long_planeteer_calls(planeteers)
   planeteers.select do |x|
     x.length > 4
   end 
+  if planateers == []
+    return false 
+  else 
+    return true
 end
 
 def find_the_cheese# code an argument here
